327895b3166256220b1604be5329de315cbeaf0e
khanhdo05 <khanhphuongdo28@gmail.com>
2024-10-05 15:33:06 -0500

    finish implementing aaccategory

--- src/AACCategory.java
+++ src/AACCategory.java
@@ -1,66 +1,87 @@
-import java.util.NoSuchElementException;
+import edu.grinnell.csc207.util.AssociativeArray;
+import edu.grinnell.csc207.util.KeyNotFoundException;
+import edu.grinnell.csc207.util.NullKeyException;
 
 /**
- * Represents the mappings for a single category of items that should
- * be displayed
+ * Represents the mappings for a single category of items that should be displayed
  * 
- * @author Catie Baker & YOUR NAME HERE
+ * @author Catie Baker & Khanh Do
  *
  */
 public class AACCategory implements AACPage {
+	/**
+	 * The name of the category.
+	 */
+	private final String categoryName;
 
-	
+	/**
+	 * The items in the category.
+	 */
+	private final AssociativeArray<String, String> items;
+
 	/**
 	 * Creates a new empty category with the given name
+	 * 
 	 * @param name the name of the category
 	 */
 	public AACCategory(String name) {
+		this.categoryName = name;
+		this.items = new AssociativeArray<>();
+	} // AACCategory(String)
 
-	}
-	
 	/**
 	 * Adds the image location, text pairing to the category
+	 * 
 	 * @param imageLoc the location of the image
 	 * @param text the text that image should speak
+	 *
+	 * @throws NullKeyException if the image location is null
 	 */
-	public void addItem(String imageLoc, String text) {
-
-	}
+	@Override
+	public void addItem(String imageLoc, String text) throws NullKeyException {
+		this.items.set(imageLoc, text);
+	} // addItem(String, String)
 
 	/**
 	 * Returns an array of all the images in the category
-	 * @return the array of image locations; if there are no images,
-	 * it should return an empty array
+	 * 
+	 * @return the array of image locations; if there are no images, it should return an empty array
 	 */
+	@Override
 	public String[] getImageLocs() {
-		return null;
-	}
+		return this.items.keys();
+	} // getImageLocs()
 
 	/**
 	 * Returns the name of the category
+	 * 
 	 * @return the name of the category
 	 */
+	@Override
 	public String getCategory() {
-		return "";
-	}
+		return this.categoryName;
+	} // getCategory()
 
 	/**
 	 * Returns the text associated with the given image in this category
+	 * 
 	 * @param imageLoc the location of the image
 	 * @return the text associated with the image
-	 * @throws NoSuchElementException if the image provided is not in the current
-	 * 		   category
+	 * @throws KeyNotFoundException if the image provided is not in the current category
 	 */
-	public String select(String imageLoc) {
-		return "";
-	}
+	@Override
+	public String select(String imageLoc) throws KeyNotFoundException {
+		return this.items.get(imageLoc);
+	} // select(String)
 
 	/**
 	 * Determines if the provided images is stored in the category
+	 * 
 	 * @param imageLoc the location of the category
 	 * @return true if it is in the category, false otherwise
 	 */
+	@Override
 	public boolean hasImage(String imageLoc) {
-		return false;
-	}
-}
+		return this.items.hasKey(imageLoc);
+	} // hasImage(String)
+} // class AACCategory
